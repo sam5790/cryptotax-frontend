925b38ad08aa448c31a60fc429bfe938ad996a1b
Dijo C G <dijocg62@gmail.com>
2025-12-02 13:06:32 +0530

    accounts-ui responsive

--- app/components/accounts/accountnew.vue
+++ app/components/accounts/accountnew.vue
@@ -1,33 +1,25 @@
 <template>
   <div class="md:p-10">
     <div class="flex flex-col justify-center items-center">
-
-      <div class="flex flex-col justify-center items-center shadow-2xl p-10 rounded-xl relative ">
+      <div class="flex flex-col justify-center items-center shadow-2xl p-10 rounded-xl relative">
         <div class="absolute -top-10 left-1/2 -translate-x-1/2">
           <img src="/avatar.png" class="w-20 h-20 rounded-full ring-4 ring-white shadow-lg" />
         </div>
         <h2 class="text-center text-xl font-medium">Total Exchanges</h2>
-        <div class="md:p-3 flex justify-center">
-
-          <div class="md:p-2 text-center">
-            <h2 class="text-xl font-medium">{{ transactions.length }}</h2>
-            <p class="">Total Transactions</p>
+        <div class="flex flex-col sm:flex-row sm:items-center sm:justify-center gap-6 sm:gap-10 mt-6 w-full">
+          <div class="text-center sm:p-2">
+            <h2 class="text-xl font-medium">
+              {{ transactions.length }}
+            </h2>
+            <p>Total Transactions</p>
           </div>
-
-          <div class="md:p-2 text-center bg-green-200 rounded-2xl">
-
+          <div class="text-center bg-green-200 rounded-2xl p-4 sm:p-2">
             <div v-if="PNL_TOTAL == 0">
-
-              <h2 class="text-xl font-medium text-green-400">
-                0
-              </h2>
+              <h2 class="text-xl font-medium text-green-400">0</h2>
             </div>
             <div v-else-if="PNL_TOTAL > 0">
-
               <h2 class="text-xl font-medium text-green-500">
-                {{
-                  PNL_TOTAL.toFixed(2)
-                }}
+                {{ PNL_TOTAL.toFixed(2) }}
               </h2>
             </div>
             <div v-else>
@@ -35,31 +27,23 @@
                 {{ PNL_TOTAL.toFixed(2) }}
               </h2>
             </div>
-
             <p>Total Account Income</p>
           </div>
-
-          <div class="md:p-2 text-center">
+          <div class="text-center sm:p-2">
             <h2 class="text-xl font-medium">01</h2>
             <p>Total Imported Files</p>
           </div>
         </div>
       </div>
-
-
-      <div class="md:min-w-[700px] md:mt-10 flex">
+      <div class="md:min-w-[700px] md:mt-10 flex max-sm:mt-5 mb-4">
         <input type="text" placeholder="Search your account"
-          class="relative w-full rounded-full border border-gray-300 px-12 py-3 shadow-sm " />
+          class="relative w-full rounded-full border border-gray-300 px-12 py-3 shadow-sm" />
       </div>
-
     </div>
 
-    <div class=" flex w-30 gap-4 md:mt-6 md:p-16 md:ml-10">
-
+    <div class="flex w-30 gap-4 md:mt-6 md:p-16 md:ml-10 max-sm:mb-5 ml-5">
       <div class="shadow-2xl rounded-2xl p-5" v-if="transactions.length > 0">
-        <div class="flex gap-4 items-center justify-between ">
-
-
+        <div class="flex gap-4 items-center justify-between">
           <!-- <div v-if="transactions[0].exchange==='CoinDCX'">
       <img src="/icons/coindcx.png" class="w-8 h-8" />
       
@@ -76,7 +60,6 @@
             <h2 class="text-lg font-medium capitalize">
               {{ transactions[0].exchange }}
             </h2>
-
           </div>
 
           <div class="rounded-md bg-teal-500 text-white px-3 py-1 text-sm shadow-sm">
@@ -89,29 +72,27 @@
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
             </svg>
           </div>
-
         </div>
 
         <div class="mt-4 ml-16">
           <h2 class="text-lg font-medium">{{ transactions.length }}</h2>
         </div>
       </div>
-
     </div>
 
-    <div @click="buttonclick" class="fixed bottom-12 right-12 bg-white border border-teal-500 rounded-xl 
-         flex items-center gap-2 px-10 py-5 shadow-md">
-      <div class="w-12 h-12 bg-teal-500 rounded-full flex items-center justify-center">
+    <div @click="buttonclick"
+      class="fixed bottom-4 right-4 sm:bottom-8 sm:right-8 bg-white border border-teal-500 rounded-xl flex items-center gap-2 px-4 sm:px-6 py-3 sm:py-4 shadow-md z-10">
+
+      <div class="w-10 h-10 sm:w-12 sm:h-12 bg-teal-500 rounded-full flex items-center justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white"
-          class="w-8 h-8">
+          class="w-6 h-6 sm:w-8 sm:h-8">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
         </svg>
       </div>
 
-      <p class="m-0 ml-5">
-        Add a new account
-      </p>
+      <p class="text-sm sm:text-base m-0 ml-3 sm:ml-5">Add a new account</p>
     </div>
+
     <Exchangeslist v-if="exchange" @close="exchange = false" @select="openDetails" />
     <addaccount v-if="showDetails" :account="selectedAccount" @close="showDetails = false" />
   </div>
@@ -119,26 +100,24 @@
 </template>
 
 <script setup>
-import Exchangeslist from '../exchangeslist.vue'
-import { mainStore } from '~/store/mainstore'
+import Exchangeslist from "../exchangeslist.vue";
+import { mainStore } from "~/store/mainstore";
 
-const store = mainStore()
-const { transactions, PNL_TOTAL } = storeToRefs(store)
+const store = mainStore();
+const { transactions, PNL_TOTAL } = storeToRefs(store);
 
-const router = useRouter()
-const exchange = ref(false)
-const showDetails = ref(false)
-const selectedAccount = ref(null)
+const router = useRouter();
+const exchange = ref(false);
+const showDetails = ref(false);
+const selectedAccount = ref(null);
 
 const buttonclick = () => {
-  exchange.value = true
-}
+  exchange.value = true;
+};
 
 function openDetails(item) {
-  selectedAccount.value = item
-  showDetails.value = true
-  exchange.value = false
+  selectedAccount.value = item;
+  showDetails.value = true;
+  exchange.value = false;
 }
-
-
 </script>
--- app/components/accounts/index.vue
+++ app/components/accounts/index.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="bg-gray-50 flex flex-col items-center ">
 
-    <div class="bg-white flex rounded-full shadow-lg p-2 gap-2 relative tabs md:mt-10">
+    <div class="bg-white flex rounded-full shadow-lg p-2 gap-2 relative tabs md:mt-10 max-sm:mb-10">
 
-      <button class=" text-center rounded-full font-medium tab-btn" :class="{ active: tab === 'tax' }"
+      <button class=" text-center rounded-full font-medium tab-btn text-base max-sm:text-xs" :class="{ active: tab === 'tax' }"
         @click="tab = 'tax'">
         Tax
       </button>
 
-      <button class="  text-center rounded-full font-medium tab-btn" :class="{ active: tab === 'transactions' }"
+      <button class="  text-center rounded-full font-medium tab-btn text-base max-sm:text-xs" :class="{ active: tab === 'transactions' }"
         @click="tab = 'transactions'">
         Transactions
       </button>
 
-      <button class="  text-center rounded-full font-medium tab-btn" :class="{ active: tab === 'accounts' }"
+      <button class="  text-center rounded-full font-medium tab-btn text-base max-sm:text-xs" :class="{ active: tab === 'accounts' }"
         @click="tab = 'accounts'">
         Accounts
       </button>
       <span class="shape"></span>
     </div>
 
-    <div class=" mt-10">
+    <div class="mt-10">
       <AccountsTax v-if="tab === 'tax'" />
       <AccountsTransactions v-if="tab === 'transactions'" />
       <AccountsAccountnew v-if="tab === 'accounts'" />
@@ -48,43 +48,48 @@ const tab = ref("tax")
 <style scoped>
 .tabs {
   position: relative;
+  display: flex;
+  width: 100%;
+  max-width: 360px;
+  justify-content: space-between;
 }
 
 .shape {
   position: absolute;
-  top: 14px;
-  left: 8px;
-  width: 128px;
-  height: 32px;
+  top: 13px;
+  left: 3px;
+  height: 30px;
   background: #027c6e;
   border-radius: 999px;
   z-index: 1;
   transition: all 0.3s ease;
+  width: calc(100% / 3 - 6px);
 }
 
-
 .tabs .tab-btn.active:nth-of-type(1)~.shape {
-  left: 8px;
+  left: 3px;
 }
 
 .tabs .tab-btn.active:nth-of-type(2)~.shape {
-  left: 144px;
+  left: calc(100% / 3 + 3px);
 }
 
 .tabs .tab-btn.active:nth-of-type(3)~.shape {
-  left: 280px;
+  left: calc((100% / 3) * 2 + 3px);
 }
 
 
+
 .tab-btn {
-  width: 128px;
+  flex: 1;
   height: 40px;
   border-radius: 999px;
   color: #555;
   font-weight: 600;
-  z-index: 2;
   position: relative;
+  z-index: 2;
   transition: color 0.3s ease;
+  text-align: center;
 }
 
 
