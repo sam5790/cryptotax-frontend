13215c1c9c8831aae916be4d63ff38c57e7628da
Nithya <nithya913k@gmail.com>
2025-12-15 17:02:34 +0530

    close on the upload

--- app/components/addaccount.vue
+++ app/components/addaccount.vue
@@ -2,7 +2,15 @@
 
   <div class="fixed inset-0 bg-black/50 grid place-items-center z-50 p-5" @click="$emit('close')">
 
-    <div class="bg-white md:p-10 rounded-2xl shadow-xl max-h-full overflow-y-auto p-8" @click.stop>
+    <div class="bg-white md:p-10 rounded-2xl shadow-xl max-h-full overflow-y-auto p-8 relative" @click.stop>
+
+ <button @click="$emit('close')" class="absolute top-0 right-0 p-3 rounded-full hover:bg-gray-200 ">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 md:w-7 md:h-7 text-white bg-[#4aabab] rounded-full" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <line x1="18" y1="6" x2="6" y2="18" />
+          <line x1="6" y1="6" x2="18" y2="18" />
+        </svg>
+      </button>
 
       <h1 class="text-xl md:text-2xl font-medium text-center mb-6">
         {{ title }}
